f4d1babb6a8a3ad7ddfe4c676a76076c1f25194c
kilogate <kilogate@163.com>
2020-08-01 12:25:12 +0800

    CyclicBarrierUsage

---
+++ hi-java/src/main/java/com/kilogate/hi/java/concurrent/synchronizer/CyclicBarrierUsage.java
@@ -0,0 +1,43 @@
+package com.kilogate.hi.java.concurrent.synchronizer;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * CyclicBarrierUsage
+ *
+ * @author fengquanwei
+ * @create 2020/7/30 上午12:05
+ **/
+public class CyclicBarrierUsage {
+    public static void main(String[] args) {
+        int parties = 5;
+
+        Runnable barrierAction = () -> {
+            System.out.println(String.format("%s 人满发车", Thread.currentThread()));
+        };
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(parties, barrierAction);
+
+        int cyclic = 3;
+        for (int i = 0; i < parties * cyclic; i++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep((long) (3000 * Math.random()));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(String.format("%s 已上车, 等待人满发车", Thread.currentThread()));
+
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
