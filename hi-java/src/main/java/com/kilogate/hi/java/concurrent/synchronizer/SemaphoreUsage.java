60683d35c3eb23ff686f9164d47c4a3f41ccb82b
kilogate <kilogate@163.com>
2020-08-01 14:16:18 +0800

    信号量的使用

---
+++ hi-java/src/main/java/com/kilogate/hi/java/concurrent/synchronizer/SemaphoreUsage.java
@@ -0,0 +1,36 @@
+package com.kilogate.hi.java.concurrent.synchronizer;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量的使用
+ *
+ * @author fengquanwei
+ * @create 2020/7/30 上午12:07
+ **/
+public class SemaphoreUsage {
+    public static void main(String[] args) {
+        int permits = 3;
+        Semaphore semaphore = new Semaphore(permits);
+
+        Runnable task = () -> {
+            try {
+                semaphore.acquire();
+
+                System.out.println(String.format("%s start", Thread.currentThread()));
+                Thread.sleep(new Random().nextInt(3000));
+                System.out.println(String.format("%s end", Thread.currentThread()));
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(task).start();
+        }
+    }
+}
