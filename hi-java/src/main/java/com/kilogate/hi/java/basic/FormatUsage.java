11b4937373a7aaae7f296543b3857463c232c192
kilogate <kilogate@163.com>
2020-08-05 00:13:45 +0800

    基本用法

---
+++ hi-java/src/main/java/com/kilogate/hi/java/basic/FormatUsage.java
@@ -0,0 +1,10 @@
+package com.kilogate.hi.java.basic;
+
+/**
+ * 格式化的用法 todo
+ *
+ * @author fengquanwei
+ * @create 2020/8/5 上午12:12
+ **/
+public class FormatUsage {
+}
---
+++ hi-java/src/main/java/com/kilogate/hi/java/basic/ScannerUsage.java
@@ -0,0 +1,13 @@
+package com.kilogate.hi.java.basic;
+
+/**
+ * Scanner 的用法 todo
+ *
+ * @author fengquanwei
+ * @create 2020/8/5 上午12:10
+ **/
+public class ScannerUsage {
+    public static void main(String[] args) {
+
+    }
+}
