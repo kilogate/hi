787732e9f6946b977d3f4391136c104370af5390
kilogate <kilogate@163.com>
2020-08-06 13:15:37 +0800

    系统相关常量

---
+++ hi-java/src/main/java/com/kilogate/hi/java/basic/SystemUsage.java
@@ -0,0 +1,14 @@
+package com.kilogate.hi.java.basic;
+
+/**
+ * 系统相关
+ *
+ * @author fengquanwei
+ * @create 2020/8/5 上午10:22
+ **/
+public class SystemUsage {
+    public static void main(String[] args) {
+        // 获取虚拟机启动目录
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
