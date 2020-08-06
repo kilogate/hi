c61c96ac443f1ad38c3437a547fe05262a173318
kilogate <kilogate@163.com>
2020-08-06 13:15:08 +0800

    文件输入输出的用法

---
+++ hi-java/src/main/java/com/kilogate/hi/java/basic/FileIoUsage.java
@@ -0,0 +1,33 @@
+package com.kilogate.hi.java.basic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * 文件输入输出的用法
+ *
+ * @author fengquanwei
+ * @create 2020/8/5 上午12:10
+ **/
+public class FileIoUsage {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filename = "/Users/fengquanwei/a.txt";
+
+        // 写文件：PrintWriter
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(filename, true));
+        printWriter.append(String.format("%s %s a%n", new Date(), Thread.currentThread()));
+        printWriter.append(String.format("%s %s b%n", new Date(), Thread.currentThread()));
+        printWriter.append(String.format("%s %s c%n", new Date(), Thread.currentThread()));
+        printWriter.close();
+
+        // 读文件：Scanner
+        Scanner scanner = new Scanner(new File(filename));
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
