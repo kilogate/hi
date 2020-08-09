05dcd6645443633c6b25e5111ec92ec410d84c1e
kilogate <kilogate@163.com>
2020-08-09 16:52:57 +0800

    映射的用法

--- hi-java/src/main/java/com/kilogate/hi/java/concurrent/collection/MapUsage.java
+++ hi-java/src/main/java/com/kilogate/hi/java/concurrent/collection/MapUsage.java
@@ -1,8 +1,13 @@
 package com.kilogate.hi.java.concurrent.collection;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
- * 映射的用法 todo
- * replace
+ * 映射的用法
+ * replaceAll
  * compute
  * merge
  *
@@ -10,4 +15,18 @@ package com.kilogate.hi.java.concurrent.collection;
  * @create 2020/8/1 下午4:03
  **/
 public class MapUsage {
+    public static void main(String[] args) {
+        Map<String, String> map = Stream.of("a", "b", "c").collect(Collectors.toMap(Function.identity(), Function.identity()));
+        System.out.println(map);
+
+        map.replaceAll((k, v) -> k + v);
+        System.out.println(map);
+
+        map.compute("a", (k, v) -> k + v);
+        System.out.println(map);
+
+        map.merge("a", "A", (k, v) -> k + v);
+        map.merge("d", "D", (k, v) -> k + v);
+        System.out.println(map);
+    }
 }
