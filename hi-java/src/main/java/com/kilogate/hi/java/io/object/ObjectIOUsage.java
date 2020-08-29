6629afa01d5f8ded8822e01d94156cc4a8856314
kilogate <kilogate@163.com>
2020-08-30 00:29:02 +0800

    对象输入输出流的用法

--- hi-java/src/main/java/com/kilogate/hi/java/io/object/ObjectIOUsage.java
+++ hi-java/src/main/java/com/kilogate/hi/java/io/object/ObjectIOUsage.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Arrays;
 
 /**
@@ -36,5 +37,69 @@ public class ObjectIOUsage {
         }
 
         System.out.println(Arrays.toString(newStaff));
+    }
+
+    private static class Employee implements Serializable {
+        String name;
+        int salary;
+
+        public Employee() {
+        }
+
+        public Employee(String name, int salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "name='" + name + '\'' +
+                    ", salary=" + salary +
+                    '}';
+        }
+    }
+
+    private static class Manager extends Employee {
+        private Employee secretary;
+
+        public Manager() {
+        }
+
+        public Manager(String name, int salary, Employee secretary) {
+            super(name, salary);
+            this.secretary = secretary;
+        }
+
+        public Employee getSecretary() {
+            return secretary;
+        }
+
+        public void setSecretary(Employee secretary) {
+            this.secretary = secretary;
+        }
+
+        @Override
+        public String toString() {
+            return "Manager{" +
+                    "secretary=" + secretary +
+                    "} " + super.toString();
+        }
     }
 }
