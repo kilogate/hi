3568c8c55dc437282a72673fe87f79369e807474
kilogate <kilogate@163.com>
2020-11-13 14:46:08 +0800

    StopWatchUsage

---
+++ hi-commons/src/main/java/com/kilogate/hi/commons/time/StopWatchUsage.java
@@ -0,0 +1,52 @@
+package com.kilogate.hi.commons.time;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * StopWatchUsage
+ *
+ * @author kilogate
+ * @create 2020/11/12 17:00
+ **/
+public class StopWatchUsage {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+
+        // task 1
+
+        stopWatch.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        stopWatch.stop();
+
+        System.out.println(stopWatch.getTime(TimeUnit.SECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.MILLISECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.MICROSECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.NANOSECONDS));
+
+        // task 2
+
+        stopWatch.reset();
+        stopWatch.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        stopWatch.stop();
+
+        System.out.println(stopWatch.getTime(TimeUnit.SECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.MILLISECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.MICROSECONDS));
+        System.out.println(stopWatch.getTime(TimeUnit.NANOSECONDS));
+    }
+}
