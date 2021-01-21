17bd9472d229312f0d3d9d713d1d941439a7be05
kilogate <kilogate@163.com>
2021-01-21 16:28:02 +0800

    logger

--- hi-logger/src/main/java/com/kilogate/hi/logger/logbackWithSlf4j/LogbackUsage.java
+++ hi-logger/src/main/java/com/kilogate/hi/logger/logbackWithSlf4j/LogbackUsage.java
@@ -32,19 +32,10 @@ public class LogbackUsage {
      * test
      */
     public static void main(String[] args) {
-        // todo ThresholdFilter
-        // <!--过滤掉spring和mybatis的一些无用的DEBUG信息 -->
-//        <logger name="org.springframework" level="INFO"></logger>
-
-//                    <!--控制台只输出level及以上级别的信息（onMatch），其他的直接拒绝（onMismatch） -->
-//
-//                    <!-- DENY，日志将立即被抛弃不再经过其他过滤器； NEUTRAL，有序列表里的下个过滤器过接着处理日志； ACCEPT，日志会被立即处理，不再经过剩余过滤器。 -->
-
-        // additivity
-
         Logger logger = LoggerFactory.getLogger(LogbackUsage.class);
-        logger.info("正常日志: {}", System.currentTimeMillis());
-        logger.error("异常日志: {}", System.currentTimeMillis());
+        logger.debug("调试信息: {}", System.currentTimeMillis());
+        logger.info("正常信息: {}", System.currentTimeMillis());
+        logger.error("异常信息: {}", System.currentTimeMillis());
 
         Logger statisticsLogger = LoggerFactory.getLogger("statisticsLogger");
         statisticsLogger.info("统计信息: {}", System.currentTimeMillis());
