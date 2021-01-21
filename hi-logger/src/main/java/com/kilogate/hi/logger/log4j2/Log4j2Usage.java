ccb98aaa1eec8fd5efdc17d53288700add1c96dc
kilogate <kilogate@163.com>
2021-01-21 16:49:42 +0800

    logger

--- hi-logger/src/main/java/com/kilogate/hi/logger/log4j2/Log4j2Usage.java
+++ hi-logger/src/main/java/com/kilogate/hi/logger/log4j2/Log4j2Usage.java
@@ -10,6 +10,26 @@ import org.apache.logging.log4j.Logger;
  * @create 2021/1/21 16:37
  **/
 public class Log4j2Usage {
+    /**
+     * <dependency>
+     * <groupId>org.apache.logging.log4j</groupId>
+     * <artifactId>log4j-api</artifactId>
+     * <version>2.8.2</version>
+     * </dependency>
+     * <dependency>
+     * <groupId>org.apache.logging.log4j</groupId>
+     * <artifactId>log4j-core</artifactId>
+     * <version>2.8.2</version>
+     * </dependency>
+     */
+
+    /**
+     * log4j2.xml
+     */
+
+    /**
+     * test
+     */
     public static void main(String[] args) {
         //             <ThresholdFilter level="info" onMatch="ACCEPT" onMismatch="DENY"/>
 
