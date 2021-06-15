699d718d9127629f092836a4ab60b9b10341a5e7
kilogate <271806949@qq.com>
2021-06-15 20:29:59 +0800

    log4j + commons-logging 的用法

--- hi-logger-log4j-jcl/src/main/java/com/kilogate/hi/logger/log4j/jcl/Log4jWithJclUsage.java
+++ hi-logger-log4j-jcl/src/main/java/com/kilogate/hi/logger/log4j/jcl/Log4jWithJclUsage.java
@@ -1,44 +1,23 @@
-package com.kilogate.hi.logger.log4jWithJcl;
+package com.kilogate.hi.logger.log4j.jcl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
+ * Log4jWithJclUsage
  * log4j + commons-logging
  *
  * @author kilogate
  * @create 2021/1/21 16:52
  **/
 public class Log4jWithJclUsage {
-    /**
-     * <dependency>
-     * <groupId>commons-logging</groupId>
-     * <artifactId>commons-logging</artifactId>
-     * <version>1.2</version>
-     * </dependency>
-     * <p>
-     * <dependency>
-     * <groupId>log4j</groupId>
-     * <artifactId>log4j</artifactId>
-     * <version>1.2.17</version>
-     * </dependency>
-     */
-
-    /**
-     * common-logging.properties
-     */
-
-    /**
-     * log4j.properties
-     */
-
-    /**
-     * test
-     */
     public static void main(String[] args) {
         Log logger = LogFactory.getLog(Log4jWithJclUsage.class);
         logger.debug("调试信息" + System.currentTimeMillis());
         logger.info("正常信息" + System.currentTimeMillis());
         logger.error("异常信息" + System.currentTimeMillis());
+
+        Log statisticsLogger = LogFactory.getLog("statisticsLogger");
+        statisticsLogger.info("统计信息" + System.currentTimeMillis());
     }
 }
