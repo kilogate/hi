da9f7898668686ba08f50611b13d5fd4d49b6ca8
kilogate <271806949@qq.com>
2021-06-15 20:18:32 +0800

    Log4j 的用法

--- hi-logger-log4j/src/main/java/com/kilogate/hi/logger/log4j/Log4jUsage.java
+++ hi-logger-log4j/src/main/java/com/kilogate/hi/logger/log4j/Log4jUsage.java
@@ -3,27 +3,12 @@ package com.kilogate.hi.logger.log4j;
 import org.apache.log4j.Logger;
 
 /**
- * log4j
+ * Log4jUsage
  *
  * @author kilogate
  * @create 2021/1/21 12:09
  **/
 public class Log4jUsage {
-    /**
-     * <dependency>
-     * <groupId>log4j</groupId>
-     * <artifactId>log4j</artifactId>
-     * <version>1.2.17</version>
-     * </dependency>
-     */
-
-    /**
-     * log4j.properties
-     */
-
-    /**
-     * test
-     */
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Log4jUsage.class);
         logger.debug("调试信息" + System.currentTimeMillis());
