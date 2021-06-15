c4094847c521be513b9d79a2439b1a1d5629244e
kilogate <271806949@qq.com>
2021-06-15 20:38:10 +0800

    logback + slf4j 的用法

--- hi-logger-logback-slf4j/src/main/java/com/kilogate/hi/logger/logback/slf4j/LogbackWithSlf4jUsage.java
+++ hi-logger-logback-slf4j/src/main/java/com/kilogate/hi/logger/logback/slf4j/LogbackWithSlf4jUsage.java
@@ -1,38 +1,18 @@
-package com.kilogate.hi.logger.logbackWithSlf4j;
+package com.kilogate.hi.logger.logback.slf4j;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * LogbackWithSlf4jUsage
  * logback + slf4j
  *
  * @author kilogate
  * @create 2021/1/20 23:05
  **/
-public class LogbackUsage {
-    /**
-     * <dependency>
-     * <groupId>org.slf4j</groupId>
-     * <artifactId>slf4j-api</artifactId>
-     * <version>1.7.25</version>
-     * </dependency>
-     * <p>
-     * <dependency>
-     * <groupId>ch.qos.logback</groupId>
-     * <artifactId>logback-classic</artifactId>
-     * <version>1.2.3</version>
-     * </dependency>
-     */
-
-    /**
-     * logback.xml
-     */
-
-    /**
-     * test
-     */
+public class LogbackWithSlf4jUsage {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(LogbackUsage.class);
+        Logger logger = LoggerFactory.getLogger(LogbackWithSlf4jUsage.class);
         logger.debug("调试信息: {}", System.currentTimeMillis());
         logger.info("正常信息: {}", System.currentTimeMillis());
         logger.error("异常信息: {}", System.currentTimeMillis());
