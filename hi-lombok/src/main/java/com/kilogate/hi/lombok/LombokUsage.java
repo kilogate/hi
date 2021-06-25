deab4309688d46af9bc18fcfa98898bb68205c58
kilogate <271806949@qq.com>
2021-06-25 11:32:01 +0800

    LombokUsage

--- hi-lombok/src/main/java/com/kilogate/hi/lombok/LombokUsage.java
+++ hi-lombok/src/main/java/com/kilogate/hi/lombok/LombokUsage.java
@@ -4,6 +4,13 @@ import lombok.extern.java.Log;
 
 /**
  * LombokUsage
+ * <p>
+ * Lombok 使用 JSR 269 Pluggable Annotation Processing API（java6 开始支持）
+ * Oracle 的 javac 支持 JSR 269
+ * <p>
+ * Maven 使用的编译器得是 Oracle javac
+ * Idea 使用的编译器得是 Oracle javac
+ * Idea 还得安装 Lombok Plugin，否则编译正常，但是显示报错
  *
  * @author kilogate
  * @create 2021/6/24 19:46
