376671c8df19dc8aa10e84a5cfd4e6b24a44530e
kilogate <271806949@qq.com>
2021-09-22 16:12:14 +0800

    java agent

--- hi-javaagent-client/src/main/java/com/kilogate/hi/javaagent/client/PreMainClient.java
+++ hi-javaagent-client/src/main/java/com/kilogate/hi/javaagent/client/PreMainClient.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  * @create 2021/9/22 14:54
  **/
 public class PreMainClient {
-    // java -javaagent:/Users/xxx/.m2/repository/com/kilogate/hi-javaagent-premain/1.0-SNAPSHOT/hi-javaagent-premain-1.0-SNAPSHOT.jar com.kilogate.hi.javaagent.client.PreMainClient hello
+    // java -javaagent:/Users/xxx/.m2/repository/com/kilogate/hi-javaagent-premain/1.0-SNAPSHOT/hi-javaagent-premain-1.0-SNAPSHOT.jar=helloagent com.kilogate.hi.javaagent.client.PreMainClient hellomain
     public static void main(String[] args) {
         System.out.println("【main】start");
         System.out.println("【main】args: " + Arrays.deepToString(args));
