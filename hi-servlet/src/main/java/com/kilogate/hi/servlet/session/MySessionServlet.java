7fab369750ded3f2951fac29ea4d549a1b3b2c23
fengquanwei <fengquanwei@corp.netease.com>
2021-12-04 20:14:00 +0800

    【servlet】session

---
+++ hi-servlet/src/main/java/com/kilogate/hi/servlet/session/MySessionServlet.java
@@ -0,0 +1,33 @@
+package com.kilogate.hi.servlet.session;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * MySessionServlet
+ *
+ * @author fengquanwei
+ * @create 2021/12/4 20:06
+ **/
+@WebServlet(urlPatterns = "/mySessionServlet")
+public class MySessionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        System.out.printf("session id: %s", session.getId());
+
+
+        Object a = session.getAttribute("a");
+        System.out.println(a);
+
+        session.setAttribute("a", "A");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("session.getId()");
+    }
+}
