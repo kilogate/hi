7ebe7a1f1e9b2b356d8446f1b8608162820c27e5
kilogate <271806949@qq.com>
2021-12-31 18:17:34 +0800

    【kafka】消费者客户端配置

--- hi-kafka/src/main/java/com/kilogate/hi/kafka/ConsumerUsage.java
+++ hi-kafka/src/main/java/com/kilogate/hi/kafka/ConsumerUsage.java
@@ -28,24 +28,35 @@ public class ConsumerUsage {
         String bootstrapServers = "localhost:9092,localhost:9093,localhost:9094";
         String topic = "topic-demo";
         String groupId = "group-demo";
+        String clientId = "consumer-demo";
 
-        // 消费者配置
+        // 一、消费者配置
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 
-        // 创建消费者实例
+        // brokers
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // key反序列化器
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // value反序列化器
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // 消费组id
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // 客户端id
+        properties.put(ConsumerConfig.CLIENT_ID_CONFIG, clientId);
+
+        // 二、创建消费者实例
         KafkaConsumer<Object, Object> kafkaConsumer = new KafkaConsumer<>(properties);
         log.info("创建消费者实例完成");
 
-        // 订阅主题
+        // 三、订阅主题
         kafkaConsumer.subscribe(Collections.singleton(topic));
         log.info("订阅主题 {} 完成", topic);
 
-        // 循环消费消息
-        while (true) {
+        // 四、循环消费消息100次
+        int index = 0;
+        while (index < 100) {
+            index++;
+
             ConsumerRecords<Object, Object> records = kafkaConsumer.poll(Duration.ofMillis(1000));
             log.info("收到 {} 条消息", records.count());
 
@@ -53,5 +64,11 @@ public class ConsumerUsage {
                 log.info("收到消息: {}", record.value());
             }
         }
+
+        // 五、提交消费位移
+        // 自动提交
+
+        // 六、关闭消费者实例
+        kafkaConsumer.close();
     }
 }
