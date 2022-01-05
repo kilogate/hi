e19ae6cf184a15b5ba3c8c18fdd23e1fa8f56358
kilogate <271806949@qq.com>
2022-01-05 20:55:09 +0800

    【kafka】查看所有主题

--- hi-kafka/src/main/java/com/kilogate/hi/kafka/AdminUsage.java
+++ hi-kafka/src/main/java/com/kilogate/hi/kafka/AdminUsage.java
@@ -1,11 +1,9 @@
 package com.kilogate.hi.kafka;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -18,10 +16,10 @@ import java.util.Properties;
 @Slf4j
 public class AdminUsage {
     public static void main(String[] args) {
-        test1();
+        test2();
     }
 
-    // 创建主贴
+    // 创建主题
     private static void test1() {
         String bootstrapServers = "localhost:9092,localhost:9093,localhost:9094";
         String topic = "topic-test1";
@@ -34,21 +32,48 @@ public class AdminUsage {
         // 二、客户端
         AdminClient adminClient = AdminClient.create(properties);
 
-        // 三、主题
+        // 主题
         NewTopic newTopic = new NewTopic(topic, 4, (short) 3);
 
-        // 四、创建主题
+        // 三、创建主题
         CreateTopicsResult result = adminClient.createTopics(Collections.singleton(newTopic));
 
         // 同步等待执行结果
         try {
             result.all().get();
-            log.info("创建主贴成功");
+            log.info("创建主题成功");
         } catch (Exception e) {
-            log.error("创建主贴异常", e);
+            log.error("创建主题异常", e);
         }
 
-        // 五、关闭客户端
+        // 四、关闭客户端
+        adminClient.close();
+    }
+
+    // 查看所有主题
+    private static void test2() {
+        String bootstrapServers = "localhost:9092,localhost:9093,localhost:9094";
+
+        // 一、配置
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(AdminClientConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
+
+        // 二、客户端
+        AdminClient adminClient = AdminClient.create(properties);
+
+        // 三、查看所有主题
+        ListTopicsResult listTopicsResult = adminClient.listTopics();
+
+        // 同步等待执行结果
+        try {
+            Collection<TopicListing> topicListings = listTopicsResult.listings().get();
+            log.info("所有主题: {}", topicListings);
+        } catch (Exception e) {
+            log.error("查看所有主题异常", e);
+        }
+
+        // 四、关闭客户端
         adminClient.close();
     }
 }
