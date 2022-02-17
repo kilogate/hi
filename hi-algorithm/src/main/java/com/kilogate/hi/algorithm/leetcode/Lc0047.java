8af34618b737fd8ffd354293a7d227a74036e39e
Lask <271806949@qq.com>
2022-02-18 00:30:59 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0047.java
@@ -0,0 +1,72 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 全排列
+ * <p>
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ *
+ * @author fengquanwei
+ * @create 2022/2/17 19:10
+ **/
+public class Lc0047 {
+    public static void main(String[] args) {
+        Lc0047 lc0047 = new Lc0047();
+        List<List<Integer>> result = lc0047.permute(new int[]{1, 1, 2, 2, 3, 3, 4});
+        result.forEach(System.out::println);
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        doPermute(nums, 0, result);
+
+        return result;
+    }
+
+    private void doPermute(int[] nums, int index, List<List<Integer>> result) {
+        if (index == nums.length - 1) {
+            result.add(buildList(nums));
+            return;
+        }
+
+        List<Integer> visited = new ArrayList<>();
+        for (int i = index; i < nums.length; i++) {
+            if (visited.contains(nums[i])) {
+                continue;
+            }
+
+            visited.add(nums[i]);
+
+            swapNum(nums, index, i);
+            doPermute(nums, index + 1, result);
+            swapNum(nums, i, index);
+        }
+    }
+
+    private void swapNum(int[] nums, int a, int b) {
+        if (a == b) {
+            return;
+        }
+
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    private List<Integer> buildList(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            result.add(num);
+        }
+
+        return result;
+    }
+}
