49990aa9ee1df1f3319d5bd39d58f0d7395bb35c
kilogate <271806949@qq.com>
2022-02-21 23:44:46 +0800

    LC

--- hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0069.java
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0069.java
@@ -1,9 +1,15 @@
 package com.kilogate.hi.algorithm.leetcode;
 
 /**
- * 全排列
+ * x 的平方根
  * <p>
- * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sqrtx
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author fengquanwei
  * @create 2022/2/18 00:10
