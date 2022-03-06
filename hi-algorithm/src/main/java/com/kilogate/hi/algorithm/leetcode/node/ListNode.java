e90280a4115707c271c84982b5cdd8f6e41a7333
kilogate <271806949@qq.com>
2022-03-06 10:41:06 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/node/ListNode.java
@@ -0,0 +1,34 @@
+package com.kilogate.hi.algorithm.leetcode.node;
+
+/**
+ * 单链表的节点
+ *
+ * @author fengquanwei
+ * @create 2022/3/6 10:34
+ **/
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+        ListNode node = this;
+        while (node != null) {
+            res.append(node.val).append("->");
+            node = node.next;
+        }
+
+        return res.substring(0, res.length() - 2);
+    }
+}
