9f273ffc01a3316427175d976c0ec4b4dc847782
kilogate <271806949@qq.com>
2022-03-12 11:14:43 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0257.java
@@ -0,0 +1,51 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+import com.kilogate.hi.algorithm.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树的所有路径
+ * <p>
+ * https://leetcode-cn.com/problems/binary-tree-paths/
+ *
+ * @author fengquanwei
+ * @create 2022/3/12 11:12
+ **/
+public class Lc0257 {
+    public static void main(String[] args) {
+        Lc0257 lc0257 = new Lc0257();
+
+        TreeNode root = TreeNode.buildTreeNode("[1,2,3,null,5]");
+        List<String> res = lc0257.binaryTreePaths(root);
+        System.out.println(res);
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList();
+        }
+
+        List<String> res = new ArrayList();
+        getBinaryTreePath(root, res, "");
+        return res;
+    }
+
+    private void getBinaryTreePath(TreeNode node, List<String> res, String solution) {
+        solution = solution + "->" + node.val;
+
+        if (node.left == null && node.right == null) {
+            res.add(solution.substring(2, solution.length()));
+            return;
+        }
+
+        if (node.left != null) {
+            getBinaryTreePath(node.left, res, solution);
+        }
+
+        if (node.right != null) {
+            getBinaryTreePath(node.right, res, solution);
+        }
+    }
+}
