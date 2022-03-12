69968af637e1fd5b207ee9bed6c819f9ad56fde1
kilogate <271806949@qq.com>
2022-03-12 15:10:10 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0401.java
@@ -0,0 +1,39 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二进制手表
+ * <p>
+ * https://leetcode-cn.com/problems/binary-watch/
+ *
+ * @author fengquanwei
+ * @create 2022/3/12 14:46
+ **/
+public class Lc0401 {
+    public static void main(String[] args) {
+        Lc0401 lc0401 = new Lc0401();
+        List<String> res = lc0401.readBinaryWatch(3);
+        System.out.println(res);
+    }
+
+    public List<String> readBinaryWatch(int turnedOn) {
+        if (turnedOn < 0 || turnedOn > 8) {
+            return new ArrayList();
+        }
+
+        List<String> res = new ArrayList<>();
+
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    String padding = m < 10 ? "0" : "";
+                    res.add(h + ":" + padding + m);
+                }
+            }
+        }
+
+        return res;
+    }
+}
