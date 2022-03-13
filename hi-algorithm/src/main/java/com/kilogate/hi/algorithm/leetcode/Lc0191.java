8ec945f54b325ad8b6a12bf1c55f10a2ca4685d5
kilogate <271806949@qq.com>
2022-03-13 15:00:03 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0191.java
@@ -0,0 +1,27 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+/**
+ * 位1的个数
+ * <p>
+ * https://leetcode-cn.com/problems/number-of-1-bits/
+ *
+ * @author fengquanwei
+ * @create 2022/3/13 14:54
+ **/
+public class Lc0191 {
+    public static void main(String[] args) {
+        Lc0191 lc0191 = new Lc0191();
+        System.out.println(lc0191.hammingWeight(123));
+    }
+
+    public int hammingWeight(int n) {
+        int res = 0;
+
+        while (n != 0) {
+            n &= n - 1;
+            res++;
+        }
+
+        return res;
+    }
+}
---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0231.java
@@ -0,0 +1,21 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+/**
+ * 2 的幂
+ * <p>
+ * https://leetcode-cn.com/problems/power-of-two/
+ *
+ * @author fengquanwei
+ * @create 2022/3/13 14:47
+ **/
+public class Lc0231 {
+    public static void main(String[] args) {
+        Lc0231 lc0231 = new Lc0231();
+        boolean res = lc0231.isPowerOfTwo(12);
+        System.out.println(res);
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+}
