966baf7f066549252c448e4957e6dfefb36e66c2
kilogate <271806949@qq.com>
2022-03-27 10:55:42 +0800

    pi

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/other/CalPi.java
@@ -0,0 +1,23 @@
+package com.kilogate.hi.algorithm.other;
+
+/**
+ * 计算圆周率
+ *
+ * @author fengquanwei
+ * @create 2022/3/27 00:33
+ **/
+public class CalPi {
+    public static void main(String[] args) {
+        System.out.println(calPi());
+    }
+
+    public static double calPi() {
+        double re = 0;
+
+        for (int i = 1; i < 10000; i++) {
+            re += ((i & 1) == 0 ? -1 : 1) * 1.0 / (2 * i - 1);
+        }
+
+        return re * 4;
+    }
+}
