97f849f9ca2f9e97296e7be4e3e3d73ddb0d70ff
Lask <271806949@qq.com>
2022-03-28 00:07:43 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0983.java
@@ -0,0 +1,50 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+/**
+ * 最低票价
+ * <p>
+ * https://leetcode-cn.com/problems/minimum-cost-for-tickets/
+ *
+ * @author fengquanwei
+ * @create 2022/2/26 23:53
+ **/
+public class Lc0983 {
+    public static void main(String[] args) {
+        Lc0983 lc0983 = new Lc0983();
+        int ans = lc0983.mincostTickets(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31}, new int[]{2, 7, 15});
+        System.out.println(ans);
+    }
+
+    public int mincostTickets(int[] days, int[] costs) {
+        int[] cache = new int[days.length];
+        return getMinCost(days, costs, cache, 0);
+    }
+
+    private int getMinCost(int[] days, int[] costs, int[] cache, int i) {
+        if (i == days.length) {
+            return 0;
+        }
+
+        if (cache[i] != 0) {
+            return cache[i];
+        }
+
+        int planA = costs[0] + getMinCost(days, costs, cache, getNextIndex(days, i, 1));
+        int planB = costs[1] + getMinCost(days, costs, cache, getNextIndex(days, i, 7));
+        int planC = costs[2] + getMinCost(days, costs, cache, getNextIndex(days, i, 30));
+
+        int min = Math.min(Math.min(planA, planB), planC);
+        cache[i] = min;
+        return min;
+    }
+
+    private int getNextIndex(int[] days, int i, int d) {
+        int max = days[i] + d;
+
+        while (i < days.length && days[i] < max) {
+            i++;
+        }
+
+        return i;
+    }
+}
---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc2028.java
@@ -0,0 +1,41 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 找出缺失的观测数据
+ * <p>
+ * https://leetcode-cn.com/problems/find-missing-observations/
+ *
+ * @author fengquanwei
+ * @create 2022/2/26 23:13
+ **/
+public class Lc2028 {
+    public static void main(String[] args) {
+        Lc2028 lc2028 = new Lc2028();
+        int[] res = lc2028.missingRolls(new int[]{3, 2, 4, 3}, 4, 2);
+        System.out.println(Arrays.toString(res));
+    }
+
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int sum = 0;
+        for (int roll : rolls) {
+            sum += mean - roll;
+        }
+        sum += mean * n;
+
+        if (sum < n || sum > n * 6) {
+            return new int[0];
+        }
+
+        int[] res = new int[n];
+
+        int quotient = sum / n;
+        int remainder = sum % n;
+        for (int i = 0; i < n; i++) {
+            res[i] = i < remainder ? quotient + 1 : quotient;
+        }
+
+        return res;
+    }
+}
