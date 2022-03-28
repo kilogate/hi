24236225698b05625cd4f4ee2925f6104e18cd8f
Lask <271806949@qq.com>
2022-03-28 23:48:33 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc0693.java
@@ -0,0 +1,21 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+/**
+ * 交替位二进制数
+ * <p>
+ * https://leetcode-cn.com/problems/binary-number-with-alternating-bits/
+ *
+ * @author fengquanwei
+ * @create 2022/3/28 23:47
+ **/
+public class Lc0693 {
+    public static void main(String[] args) {
+        boolean res = new Lc0693().hasAlternatingBits(5);
+        System.out.println(res);
+    }
+
+    public boolean hasAlternatingBits(int n) {
+        int a = n ^ (n >>> 1);
+        return (a & (a + 1)) == 0;
+    }
+}
