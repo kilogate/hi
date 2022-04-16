80fb72447d2713a75b6df40463189ee4352bd71d
kilogate <271806949@qq.com>
2022-04-16 12:39:17 +0800

    LC

---
+++ hi-algorithm/src/main/java/com/kilogate/hi/algorithm/leetcode/Lc1672.java
@@ -0,0 +1,23 @@
+package com.kilogate.hi.algorithm.leetcode;
+
+/**
+ * 最富有客户的资产总量
+ * <p>
+ * https://leetcode-cn.com/problems/richest-customer-wealth/
+ *
+ * @author fengquanwei
+ * @create 2022/4/16 12:38
+ **/
+public class Lc1672 {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] arr : accounts) {
+            int sum = 0;
+            for (int account : arr) {
+                sum += account;
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
