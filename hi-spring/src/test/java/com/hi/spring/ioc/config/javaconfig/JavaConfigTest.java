ebbc28220410f24759434e1b2731d09ad2bb4583
kilogate <271806949@qq.com>
2023-10-08 10:23:53 +0800

    1

---
+++ hi-spring/src/test/java/com/hi/spring/ioc/config/javaconfig/JavaConfigTest.java
@@ -0,0 +1,25 @@
+package com.hi.spring.ioc.config.javaconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * JavaConfigTest
+ *
+ * @author fengquanwei
+ * @create 2023/10/8 10:21
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Config.class)
+public class JavaConfigTest {
+    @Autowired
+    private ServiceA serviceA;
+
+    @Test
+    public void test() {
+        serviceA.hello();
+    }
+}
