f41696a6b2d6b158884e6c34b8ae84f39e499d81
kilogate <271806949@qq.com>
2023-10-09 10:23:09 +0800

    1

---
+++ hi-spring/src/main/java/com/hi/spring/ioc/ambiguity/primary/ServiceB.java
@@ -0,0 +1,13 @@
+package com.hi.spring.ioc.ambiguity.primary;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ServiceB
+ *
+ * @author fengquanwei
+ * @create 2023/10/9 10:20
+ **/
+@Component
+public class ServiceB implements Service {
+}
