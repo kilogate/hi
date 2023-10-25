6d95087118b07ecaec61de6be5d5a9be05093e30
kilogate <271806949@qq.com>
2023-10-25 14:48:30 +0800

    1

---
+++ hi-spring/src/main/java/com/hi/spring/ioc/placeholder/Config.java
@@ -0,0 +1,28 @@
+package com.hi.spring.ioc.placeholder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * Config
+ *
+ * @author fengquanwei
+ * @create 2023/10/25 14:16
+ **/
+@Configuration
+@ComponentScan
+@PropertySource("classpath:ioc/property/app.properties") // 声明属性源
+public class Config {
+    /**
+     * 属性源占位符配置器
+     *
+     * @return
+     */
+    @Bean
+    public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
---
+++ hi-spring/src/main/java/com/hi/spring/ioc/placeholder/Main.java
@@ -0,0 +1,18 @@
+package com.hi.spring.ioc.placeholder;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Main
+ *
+ * @author fengquanwei
+ * @create 2023/10/25 14:25
+ **/
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
+        Service service = applicationContext.getBean(Service.class);
+        System.out.println(service);
+    }
+}
