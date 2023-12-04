ec3f5f436f2c026f730228538c7c7733cc70ed29
kilogate <271806949@qq.com>
2023-12-04 15:50:04 +0800

    1

---
+++ hi-golang/demo/util/string_util.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/apaxa-go/helper/strconvh"
+	"hi-golang/demo/logger"
+)
+
+func ToBytes(ctx context.Context, v interface{}) []byte {
+	data, err := json.Marshal(v)
+	if err != nil {
+		logger.Error(ctx, "ToBytes err, json.Marshal err, v: %+v, err: %+v", v, err)
+		return nil
+	}
+
+	return data
+}
+
+func ToString(ctx context.Context, v interface{}) string {
+	data, err := json.Marshal(v)
+	if err != nil {
+		logger.Error(ctx, "ToString err, json.Marshal err, v: %+v, err: %+v", v, err)
+		return ""
+	}
+
+	return string(data)
+}
+
+func ParseInt(ctx context.Context, s string) int64 {
+	if s == "" {
+		logger.Error(ctx, "ParseInt fail, empty param")
+		return 0
+	}
+
+	res, err := strconvh.ParseInt64(s)
+	if err != nil {
+		logger.Error(ctx, "ParseInt err, strconvh.ParseInt64 err, s: %s, err: %+v", s, err)
+		return 0
+	}
+
+	return res
+}
